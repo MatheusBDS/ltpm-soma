5012b4ff2c9c1bf5cab92700fefd60ad53d687d7
Matheus Barbosa <matheusb565@gmail.com>
2020-03-07 11:27:24 -0300

    Criação dos Atributos para Realização da Soma

--- app/src/main/java/com/example/soma/MainActivity.java
+++ app/src/main/java/com/example/soma/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.soma;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickSomar(View view) {
+        // Obter valor 1
+        EditText v1 = (EditText) findViewById(R.id.n1);
+        // Obter valor 2
+        EditText v2 = (EditText) findViewById(R.id.n2);
+
+        // Converte para float e soma
+        float r = Float.parseFloat(v1.getText().toString()) + Float.parseFloat(v2.getText().toString());
+
+        // Obter Resultado
+        TextView resultado = (TextView) findViewById(R.id.resultado);
+
+        // Exibe o resultado
+        resultado.setText("Resultado: " + r);
     }
 }
